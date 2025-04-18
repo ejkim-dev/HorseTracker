581a4d1bf35691243e4111cc8e8db4a5a14b78b8
Eunjin Kim <biz.ejkim@gmail.com>
2025-04-18 14:40:49 +0900

    fix : MainActivity에 있는 로직 부분 ViewModel로 이동

--- app/src/main/java/com/example/horsetracker/presentation/MainActivity.kt
+++ app/src/main/java/com/example/horsetracker/presentation/MainActivity.kt
@@ -1,49 +1,28 @@
 package com.example.horsetracker.presentation
 
-import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.LaunchedEffect
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.aitracker.api.DetectionBox
-import com.example.aitracker.api.Detector
-import com.example.aitracker.api.DetectorFactory
-import com.example.horsetracker.presentation.util.CameraController
 import com.example.horsetracker.presentation.feature.camera.CameraPermissionCheckSnackbar
 import com.example.horsetracker.presentation.feature.camera.CameraViewModel
-import com.example.horsetracker.presentation.util.CameraXController
 import com.example.horsetracker.presentation.theme.HorseTrackerTheme
 
-class MainActivity : ComponentActivity(), Detector.DetectionListener,
-    CameraController.CameraFrameProcessor {
-
-    private lateinit var cameraViewModel: CameraViewModel
-    private val horseDetector: Detector by lazy {
-        DetectorFactory.createHorseDetector(
-            context = baseContext,
-            listener = this
-        )
-    }
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        horseDetector.setup()
 
         setContent {
-            cameraViewModel = viewModel()
+            val cameraViewModel: CameraViewModel = viewModel()
 
             LaunchedEffect(Unit) {
-                cameraViewModel.apply {
-                    initCameraController(
-                        CameraXController(
-                            context = baseContext,
-                            lifecycleOwner = this@MainActivity,
-                            cameraFrameProcessor = this@MainActivity
-                        )
-                    )
-                }
+                cameraViewModel.initialize(
+                    context = baseContext,
+                    lifecycleOwner = this@MainActivity
+                )
             }
 
             HorseTrackerTheme(
@@ -56,26 +35,6 @@ class MainActivity : ComponentActivity(), Detector.DetectionListener,
                 }
             }
         }
-    }
-
-    override fun detectionResult(results: List<DetectionBox>) {
-        cameraViewModel.updateBoundingBoxes(results)
-    }
-
-    override fun emptyDetection() {
-        cameraViewModel.updateBoundingBoxes(emptyList())
-    }
-
-    override fun detectionError(e: Exception) {
-        cameraViewModel.updateBoundingBoxes(emptyList())
-    }
-
-    override fun processFrame(frame: Bitmap) {
-        horseDetector.detect(frame)
-    }
-
-    override fun processError(exception: Exception) {
-        cameraViewModel.updateBoundingBoxes(emptyList())
     }
 }
 
--- app/src/main/java/com/example/horsetracker/presentation/feature/camera/CameraViewModel.kt
+++ app/src/main/java/com/example/horsetracker/presentation/feature/camera/CameraViewModel.kt
@@ -1,13 +1,19 @@
 package com.example.horsetracker.presentation.feature.camera
 
+import android.content.Context
+import android.graphics.Bitmap
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
 import com.example.aitracker.api.DetectionBox
+import com.example.aitracker.api.Detector
+import com.example.aitracker.api.DetectorFactory
 import com.example.horsetracker.presentation.feature.uistate.CameraState
 import com.example.horsetracker.presentation.util.CameraController
+import com.example.horsetracker.presentation.util.CameraXController
 
-class CameraViewModel: ViewModel() {
+class CameraViewModel: ViewModel(), Detector.DetectionListener, CameraController.CameraFrameProcessor  {
     private val _cameraState = mutableStateOf<CameraState>(CameraState.PermissionDenied)
     val cameraState: State<CameraState> = _cameraState
 
@@ -15,23 +21,56 @@ class CameraViewModel: ViewModel() {
     val cameraController: CameraController
         get() = _cameraController ?: throw IllegalStateException("Camera not initialized")
 
-
     private val _boundingBoxes = mutableStateOf<List<DetectionBox>>(emptyList())
     val boundingBoxes: State<List<DetectionBox>> = _boundingBoxes
+
+    private lateinit var horseDetector: Detector
 
     override fun onCleared() {
         super.onCleared()
         _cameraController?.shutdown()
-    }
-
-    fun initCameraController(cameraController: CameraController) {
-        if (_cameraController == null) {
-            _cameraController = cameraController
+        if (::horseDetector.isInitialized) {
+            horseDetector.clear()
         }
     }
 
-    fun updateBoundingBoxes(newBoxes: List<DetectionBox>) {
-        _boundingBoxes.value = newBoxes
+    override fun detectionResult(results: List<DetectionBox>) {
+        updateBoundingBoxes(results)
+    }
+
+    override fun emptyDetection() {
+        updateBoundingBoxes(emptyList())
+    }
+
+    override fun detectionError(e: Exception) {
+        updateBoundingBoxes(emptyList())
+    }
+
+    override fun processFrame(frame: Bitmap) {
+        horseDetector.detect(frame)
+    }
+
+    override fun processError(exception: Exception) {
+        updateBoundingBoxes(emptyList())
+    }
+
+    // 카메라와 Detector 설정
+    fun initialize(context: Context, lifecycleOwner: LifecycleOwner) {
+        // Detector 초기화
+        horseDetector = DetectorFactory.createHorseDetector(
+            context = context,
+            listener = this
+        )
+        horseDetector.setup()
+
+        // CameraController 초기화
+        initCameraController(
+            CameraXController(
+                context = context,
+                lifecycleOwner = lifecycleOwner,
+                cameraFrameProcessor = this
+            )
+        )
     }
 
     fun onPermissionResult(isGranted: Boolean) {
@@ -44,5 +83,15 @@ class CameraViewModel: ViewModel() {
 
     fun onCameraInitialized() {
         _cameraState.value = CameraState.PermissionGranted
+    }
+
+    private fun initCameraController(cameraController: CameraController) {
+        if (_cameraController == null) {
+            _cameraController = cameraController
+        }
+    }
+
+    private fun updateBoundingBoxes(newBoxes: List<DetectionBox>) {
+        _boundingBoxes.value = newBoxes
     }
 }
